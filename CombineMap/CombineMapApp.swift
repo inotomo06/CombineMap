70a5df3ad2606b5dd13fb5d43d103f8c176042e3
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-07-12 21:52:06 +0900

    Initial Commit

---
+++ CombineMap/CombineMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMapApp.swift
+//  CombineMap
+//
+//  Created by cmStudent on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct CombineMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
